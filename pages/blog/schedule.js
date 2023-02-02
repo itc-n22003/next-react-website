30a2705ef0378c6457ec325ff2329aa81ee7bbf9
n22003 <n22003@std.it-college.ac.jp>
2023-02-02 07:32:59 +0000

    chap8

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,18 +1,58 @@
 import { getPostBySlug } from 'lib/api'
+import { extractText } from 'lib/extract-text'
+import Meta from 'component/meta'
 import Container from 'component/container'
+import PostHeader from 'component/post-header'
+import PostBody from 'component/post-body'
+import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'component/two-column'
+import ConvertBody from 'component/convert-body'
+import PostCategories from 'component/post-categories'
+import Image from 'next/image'
 
-export default function Schedule ({ title, publish, content, eyecatch, categories }) {
+export default function Schedule ({ title, publish, content, eyecatch, categories, description }) {
   return (
     <Container>
-      <h1>{title}</h1>
+      <Meta
+        pageTitie={title}
+        pageDesc={description}
+        pageImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.hright}
+      />
+      <article>
+        <PostHeader title={title} subtitle='Blog Article' publish={publish} />
+        <figure>
+          <Image
+            src={eyecatch.url}
+            alt=''
+            layout='responsive'
+            width={eyecatch.width}
+            height={eyecatch.height}
+            sizes='(min-width: 1152px) 1152px, 100vw'
+            priority
+          />
+        </figure>
+
+        <TwoColumn>
+          <TwoColumnMain>
+            <PostBody>
+
+              <ConvertBody contentHTML={content} />
+            </PostBody>
+          </TwoColumnMain>
+          <TwoColumnSidebar>
+            <PostCategories categories={categories} />
+          </TwoColumnSidebar>
+        </TwoColumn>
+      </article>
     </Container>
   )
 }
 
 export async function getStaticProps () {
   const slug = 'schedule'
-
   const post = await getPostBySlug(slug)
+  const description = extractText(post.content)
 
   return {
     props: {
@@ -20,7 +60,8 @@ export async function getStaticProps () {
       publish: post.publishDate,
       content: post.content,
       eyecatch: post.eyecatch,
-      categories: post.categories
+      categories: post.categories,
+      description
     }
   }
 }
