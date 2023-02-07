8358e9e64becb3517493191bd3b57c9a81d6ea9f
n22003 <n22003@std.it-college.ac.jp>
2023-02-07 06:42:07 +0000

    chap9.3

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,4 +1,4 @@
-import { getPostBySlug } from 'lib/api'
+import { getPostBySlug, getAllSlugs } from 'lib/api'
 import { extractText } from 'lib/extract-text'
 import Meta from 'component/meta'
 import Container from 'component/container'
@@ -8,8 +8,10 @@ import { TwoColumn, TwoColumnMain, TwoColumnSidebar } from 'component/two-column
 import ConvertBody from 'component/convert-body'
 import PostCategories from 'component/post-categories'
 import Image from 'next/image'
+import { eyecatchLocal } from 'lib/constants'
+import { getPlaiceholder } from 'plaiceholder'
 
-export default function Schedule ({ title, publish, content, eyecatch, categories, description }) {
+const Post = ({ title, publish, content, eyecatch, categories, description }) => {
   return (
     <Container>
       <Meta
@@ -30,6 +32,8 @@ export default function Schedule ({ title, publish, content, eyecatch, categorie
             height={eyecatch.height}
             sizes='(min-width: 1152px) 1152px, 100vw'
             priority
+            placeholder='blur'
+            blurDataURL={eyecatch.blurDataURL}
           />
         </figure>
 
@@ -49,17 +53,31 @@ export default function Schedule ({ title, publish, content, eyecatch, categorie
   )
 }
 
-export async function getStaticProps () {
-  const slug = 'schedule'
+export default Post
+
+export async function getStaticPaths () {
+  const allSlugs = await getAllSlugs()
+
+  return {
+    paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
+    fallback: false
+  }
+}
+
+export async function getStaticProps (context) {
+  const slug = context.params.slug
   const post = await getPostBySlug(slug)
   const description = extractText(post.content)
+  const eyecatch = post.eyecatch ?? eyecatchLocal
 
+  const { base64 } = await getPlaiceholder(eyecatch.url)
+  eyecatch.blurDataURL = base64
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch,
       categories: post.categories,
       description
     }
