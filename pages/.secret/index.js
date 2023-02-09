cb02d9ff52a6a22ddbf52670fa74ef6a76e6137b
n22003 <n22003@std.it-college.ac.jp>
2023-02-09 08:01:11 +0000

    chap10

--- pages/.secret/index.js
+++
@@ -1,8 +0,0 @@
-const Secret = () => {
-  return (
-    <>
-      <p>TEST</p>
-    </>
-  )
-}
-export default Secret
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import 'styles/globals.css'
 import Layout from 'component/layout'
+import Script from 'next/script'
+import * as gtag from 'lib/gtag'
 
 // Fonr Awesome の設定
 import '@fortawesome/fontawesome-svg-core/styles.css'
@@ -7,10 +11,38 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
 
 const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy='afterInteractive'
+        src={`https"://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id='gtag'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Data());
+      gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+      `
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 export default MyApp
